940748fc1ccddbf54606f386f267e882e6eccc08
nathanop11 <nathanop_123@hotmail.com>
2024-06-13 01:24:51 -0300

    Criação calendario e agenda

--- app/src/main/java/br/com/fiap/challengedeemail/MainActivity.kt
+++ app/src/main/java/br/com/fiap/challengedeemail/MainActivity.kt
@@ -4,19 +4,26 @@ import br.com.fiap.challengedeemail.screens.HomeScreen
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import br.com.fiap.challengedeemail.screens.AnotacoesScreen
 import br.com.fiap.challengedeemail.screens.FormLogin
-import br.com.fiap.challengedeemail.ui.theme.ChallengeDeEmailTheme
+import com.pandaways.mycalendar.ui.CalendarApp
+import com.pandaways.mycalendar.ui.theme.MyCalendarTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            AnotacoesScreen(context = this)
+            MyCalendarTheme {
+                CalendarApp(
+                    modifier = Modifier.padding(16.dp)
+                )
+            }
         }
     }
 }
@@ -29,10 +36,3 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ChallengeDeEmailTheme {
-        Greeting("Android")
-    }
-}
--- app/src/main/java/br/com/fiap/challengedeemail/ui/theme/Theme.kt
+++ app/src/main/java/br/com/fiap/challengedeemail/ui/theme/Theme.kt
@@ -1,19 +1,22 @@
-package br.com.fiap.challengedeemail.ui.theme
+package com.pandaways.mycalendar.ui.theme
 
 import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
-import androidx.core.view.WindowCompat
+import androidx.core.view.ViewCompat
+import br.com.fiap.challengedeemail.ui.theme.Pink40
+import br.com.fiap.challengedeemail.ui.theme.Pink80
+import br.com.fiap.challengedeemail.ui.theme.Purple40
+import br.com.fiap.challengedeemail.ui.theme.Purple80
+import br.com.fiap.challengedeemail.ui.theme.PurpleGrey40
+import br.com.fiap.challengedeemail.ui.theme.PurpleGrey80
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
@@ -24,9 +27,7 @@ private val DarkColorScheme = darkColorScheme(
 private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
+    tertiary = Pink40,
     background = Color(0xFFFFFBFE),
     surface = Color(0xFFFFFBFE),
     onPrimary = Color.White,
@@ -34,11 +35,10 @@ private val LightColorScheme = lightColorScheme(
     onTertiary = Color.White,
     onBackground = Color(0xFF1C1B1F),
     onSurface = Color(0xFF1C1B1F),
-    */
 )
 
 @Composable
-fun ChallengeDeEmailTheme(
+fun MyCalendarTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
@@ -49,22 +49,19 @@ fun ChallengeDeEmailTheme(
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
-
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
     val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
-            val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+            (view.context as Activity).window.statusBarColor = colorScheme.primary.toArgb()
+            ViewCompat.getWindowInsetsController(view)?.isAppearanceLightStatusBars = darkTheme
         }
     }
 
     MaterialTheme(
         colorScheme = colorScheme,
-        typography = Typography,
         content = content
     )
 }
